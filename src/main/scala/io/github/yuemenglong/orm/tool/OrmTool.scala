cb876519a5751c2dd84a21cc51b34746d925d046
yuemenglong <yuemenglong@126.com>
2018-01-04 11:16:22 +0800

    add: 默认不导出初始化代码

--- src/main/scala/io/github/yuemenglong/orm/tool/OrmTool.scala
+++ src/main/scala/io/github/yuemenglong/orm/tool/OrmTool.scala
@@ -30,91 +30,20 @@ object OrmTool {
     }).toMap
   }
 
-  def exportTsClass3(path: String): Unit = {
-    var referMap: Map[String, Set[String]] = Map()
-    val checkWithMap = (meta: EntityMeta) => {
-      require(!referMap.contains(meta.entity))
-      val entity = meta.entity
-      // 所有一对一的类型
-      val refers = meta.fieldMap.values.filter(f => f.isPointer || f.isOneOne)
-        .map(_.asInstanceOf[FieldMetaRefer].refer.entity)
-      // 1. 获取不能new的字段(对方的展开结合里包含该类型,说明对方能new出该类型)
-      val invalids = refers.filter(r => referMap.get(r) match {
-        case Some(s) => s.contains(entity)
-        case None => false
-      }).toArray ++ Array(entity)
-      // 2. 所有能new的字段(即一对一的)，打平set
-      val set: Set[String] = refers.filter(!invalids.contains(_))
-        .flatMap(e => referMap.get(e) match {
-          case Some(s) => s ++ Set(e)
-          case None => Set(e)
-        }).toSet
-      // 3. 所有已经存在的实体，且能new出当前类型的，将当前的集合并入
-      referMap.mapValues(s => {
-        if (s.contains(entity)) {
-          s ++ set
-        } else {
-          s
-        }
-      })
-      referMap += (entity -> set)
-      invalids
-    }
-    val checkInvalid = (invalids: Array[String], field: FieldMetaRefer) => {
-      if (invalids.contains(field.refer.entity)) {
-        s"${field.refer.entity} = undefined"
-      } else {
-        s"${field.refer.entity} = new ${field.refer.entity}()"
-      }
-    }
-    val classes = OrmMeta.entityVec.map(meta => {
-      val invalids = checkWithMap(meta)
-      val fields = meta.fieldVec.filter(field => {
-        field match {
-          case _: FieldMetaFkey => false
-          case _ => true
-        }
-      }).map(field => {
-        val name = field.name
-        val ty = field match {
-          case _: FieldMetaBoolean => "boolean = undefined"
-          case _: FieldMetaInteger => "number = undefined"
-          case _: FieldMetaSmallInt => "number = undefined"
-          case _: FieldMetaTinyInt => "number = undefined"
-          case _: FieldMetaLong => "number = undefined"
-          case _: FieldMetaDouble => "number = undefined"
-          case _: FieldMetaFloat => "number = undefined"
-          case _: FieldMetaDecimal => "number = undefined"
-
-          case _: FieldMetaString => "string = undefined"
-          case _: FieldMetaEnum => "string = undefined"
-          case _: FieldMetaLongText => "string = undefined"
-          case _: FieldMetaDate => "string = undefined"
-          case _: FieldMetaDateTime => "string = undefined"
-
-          case field: FieldMetaPointer => checkInvalid(invalids, field)
-          case field: FieldMetaOneOne => checkInvalid(invalids, field)
-          case field: FieldMetaOneMany => s"Array<${field.refer.entity}> = []"
-        }
-        s"\t$name: $ty;"
-      }).mkString("\n")
-      s"export class ${meta.entity} {\n$fields\n}"
-    }).mkString("\n\n")
-    val fs = new FileOutputStream(path)
-    fs.write(classes.getBytes())
-    fs.close()
+  def exportTsClass(path: String): Unit = {
+    exportTsClass(path, init = false)
   }
 
-  def exportTsClass(path: String): Unit = {
+  def exportTsClass(path: String, init: Boolean): Unit = {
     val relateMap = mutable.Map[String, Set[String]]()
-    val classes = OrmMeta.entityVec.map(e => stringifyTsClass(e.clazz, relateMap)).mkString("\n\n")
+    val classes = OrmMeta.entityVec.map(e => stringifyTsClass(e.clazz, relateMap, init)).mkString("\n\n")
     val fs = new FileOutputStream(path)
     fs.write(classes.getBytes())
     fs.close()
   }
 
   // relateMap保存所有与之相关的类型
-  def stringifyTsClass(clazz: Class[_], relateMap: mutable.Map[String, Set[String]]): String = {
+  def stringifyTsClass(clazz: Class[_], relateMap: mutable.Map[String, Set[String]], init: Boolean): String = {
     val className = clazz.getSimpleName
     val newFields = new ArrayBuffer[Field]()
     val content = Kit.getDeclaredFields(clazz).map(f => {
@@ -133,16 +62,20 @@ object OrmTool {
         case _ => f.getType.isArray match {
           case true => s"$typeName = []" // 数组
           case false => // 非数组
-            relateMap.contains(typeName) match {
-              case false => // 该类型还没有导出过，安全的
-                newFields += f
-                s"$typeName = new $typeName()"
-              case true => relateMap(typeName).contains(className) match { // 导出过
-                case false => // 该类型与自己没有关系，安全的
-                  newFields += f
-                  s"$typeName = new $typeName()"
-                case true => s"$typeName = undefined" // 自己已经被该类型导出过，避免循环引用
-              }
+            init match {
+              case false => s"$typeName = undefined"
+              case true => //需要初始化
+                relateMap.contains(typeName) match {
+                  case false => // 该类型还没有导出过，安全的
+                    newFields += f
+                    s"$typeName = new $typeName()"
+                  case true => relateMap(typeName).contains(className) match { // 导出过
+                    case false => // 该类型与自己没有关系，安全的
+                      newFields += f
+                      s"$typeName = new $typeName()"
+                    case true => s"$typeName = undefined" // 自己已经被该类型导出过，避免循环引用
+                  }
+                }
             }
         }
       }
