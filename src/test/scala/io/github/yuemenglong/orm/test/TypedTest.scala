befa535d669b240e09544e5d63e0e2c39590cf35
yuemenglong <yuemenglong@126.com>
2018-01-31 18:43:03 +0800

    add: 补充部分单元测试

--- src/test/scala/io/github/yuemenglong/orm/test/TypedTest.scala
+++ src/test/scala/io/github/yuemenglong/orm/test/TypedTest.scala
@@ -3,7 +3,7 @@ package io.github.yuemenglong.orm.test
 import io.github.yuemenglong.orm.Orm
 import io.github.yuemenglong.orm.db.Db
 import io.github.yuemenglong.orm.test.entity._
-import org.junit.{After, Before, Test}
+import org.junit.{After, Assert, Before, Test}
 
 /**
   * Created by <yuemenglong@126.com> on 2018/1/31.
@@ -49,6 +49,69 @@ class TypedTest {
       ex.insert()(_.om)
       session.execute(ex)
     })
+    val root = Orm.root(classOf[Obj])
+    val cond = root.get(_.id).lt(2)
+      .or(root.get(_.id).gt(9))
+      .and(root.selects(_.om).get(_.id).gt(2))
+    val objs = session.query(Orm.selectFrom(root).where(cond))
+    Assert.assertEquals(objs.length, 2)
+    Assert.assertEquals(objs(0).om.length, 1)
+    Assert.assertEquals(objs(1).om.length, 3)
   })
 
+  @Test
+  def testNotNull(): Unit = db.beginTransaction(session => {
+    var obj = new Obj
+    obj.name = "name"
+    session.execute(Orm.insert(Orm.convert(obj)))
+
+    obj = new Obj
+    obj.name = "name2"
+    obj.age = 10
+    session.execute(Orm.insert(Orm.convert(obj)))
+
+    val root = Orm.root(classOf[Obj])
+    var res = session.query(Orm.selectFrom(root).where(root.get(_.age).isNull))
+    Assert.assertEquals(res.length, 1)
+    Assert.assertEquals(res(0).id.longValue(), 1)
+    Assert.assertEquals(res(0).age, null)
+
+    res = session.query(Orm.selectFrom(root).where(root.get(_.age).notNull()))
+    Assert.assertEquals(res.length, 1)
+    Assert.assertEquals(res(0).id.longValue(), 2)
+    Assert.assertEquals(res(0).age, 10)
+  })
+
+  @Test
+  def testCount(): Unit = db.beginTransaction(session => {
+    val obj = new Obj
+    obj.name = ""
+    obj.om = Array(new OM, new OM, new OM, new OM, new OM, new OM)
+    val ex = Orm.insert(Orm.convert(obj))
+    ex.insert(_.ptr)
+    ex.insert(_.oo)
+    ex.inserts(_.om)
+    val ret = session.execute(ex)
+    Assert.assertEquals(ret, 7)
+
+    val root = Orm.root(classOf[OM])
+    val c = session.first(Orm.select(root.count()).from(root))
+    Assert.assertEquals(c.longValue(), 6)
+  })
+
+  @Test
+  def testField(): Unit = db.beginTransaction(session => {
+    val obj = new Obj
+    obj.name = ""
+    obj.om = Array(new OM, new OM, new OM, new OM, new OM, new OM)
+
+    val ex = Orm.insert(Orm.convert(obj))
+    ex.insert()(_.om)
+    session.execute(ex)
+
+    val root = Orm.root(classOf[Obj])
+    val c = session.first(Orm.select(root.join()(_.om).get(_.id).asLong())
+      .from(root).limit(1).offset(5))
+    Assert.assertEquals(c.intValue(), 6)
+  })
 }
